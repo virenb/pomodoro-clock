56708522af0e76d56a1ccfb7b5bc59f59dc20c57
Viren Bhagat <virenb@gmail.com>
2017-10-23 18:58:37 -0400

    Cleaned up increase and decrease functions, created global variables

--- app.js
+++ app.js
@@ -1,68 +1,47 @@
-function incrementSession() {
-  var value = parseInt(document.getElementById('session').value, 10);
-  value = isNaN(value) ? 0 : value;
-  value++;
-  document.getElementById('session').value = value;
-  document.getElementById('clock').innerHTML = value;
-}
+var activeSession = false;
+var activeBreak = false;
+var sessionMinutes = parseInt(document.getElementById('sessionLength').innerHTML);
+var breakMinutes = parseInt(document.getElementById('breakMinutes').innerHTML);
+var sessionSeconds = 0;
+var breakSeconds = 0;
+
+function increaseSession() {
+  if (!activeSession) {
+    var value = parseInt(document.getElementById('sessionLength').innerHTML);
+    value++;
+    sessionMinutes = value;
+    document.getElementById('sessionLength').innerHTML = value;
+  }
+};
 
 function decreaseSession() {
-  var value = parseInt(document.getElementById('session').value, 10);
-  value = isNaN(value) ? 0 : value;
-  value--;
-  if (value <= 0) {
-    value = 0;
+  if (!activeSession) {
+    var value = parseInt(document.getElementById('sessionLength').innerHTML);
+    if (value !== 0) {
+      value--;
+      sessionMinutes = value;
+      document.getElementById('sessionLength').innerHTML = value;
+    }
   }
-  document.getElementById('session').value = value;
-  document.getElementById('clock').innerHTML = value;
-}
+};
 
-function incrementBreak() {
-  var value = parseInt(document.getElementById('break').value, 10);
-  value = isNaN(value) ? 0 : value;
-  value++;
-  document.getElementById('break').value = value;
-}
+function increaseBreak() {
+  if (!activeBreak) {
+    var value = parseInt(document.getElementById('breakLength').innerHTML);
+    value++;
+    breakMinutes = value;
+    document.getElementById('breakLength').innerHTML = value;
+  }
+};
 
 function decreaseBreak() {
-  var value = parseInt(document.getElementById('break').value, 10);
-  value = isNaN(value) ? 0 : value;
-  value--;
-  if (value <= 0) {
-    value = 0;
+  if (!activeBreak) {
+    var value = parseInt(document.getElementById('breakLength').innerHTML);
+    if (value !== 0) {
+      value--;
+      breakMinutes = value;
+      document.getElementById('breakLength').innerHTML = value;
+    }
   }
-  document.getElementById('break').value = value;
-}
+};
 
-document.getElementById('clock').innerHTML = document.getElementById('session');
-
-// Make a function to start the counter
-// Has to count down from the value of session
-// Has to display mm:ss
-
-var current, initial, onPause;
-var times = { "current": current, "initial": initial };
-
-function start() {
-  onBreak = false;
-  times.initial = +document.getElementById('session').innerHTML * 60;
-  times.current = +document.getElementById('session').innerHTML * 60;
-  startInterval = setInterval(countdown, 1000);
-}
-
-function startBreak() {
-  times.initial = +document.getElementById('break').innerHTML * 60;
-  current = +document.getElementById('break').innerHTML * 60;
-  onBreak = true;
-  startInterval = setInterval(countdown, 1000);
-}
-
-function reset() {
-  document.getElementById('session').value = 25;
-  document.getElementById('clock').innerHTML = 25;
-  document.getElementById('break').value = 5;
-}
-
-function pause() {
-
-}
