5bb4c7c9f93040a9edfbc4801254a78daf3f8867
Viren Bhagat <virenb@gmail.com>
2017-10-24 21:01:56 -0400

    Using jQuery now

--- app.js
+++ app.js
@@ -1,105 +1,45 @@
-var activeSession = false;
-var activeBreak = false;
-var sessionMinutes = parseInt(document.getElementById('sessionLength').innerHTML);
-var breakMinutes = parseInt(document.getElementById('breakMinutes').innerHTML);
-var sessionSeconds = 0;
-var breakSeconds = 0;
+$(document).ready(function() {
+  var clock = parseInt($("#clock").html());
+  var sessionMinutes = parseInt($("#sessionLength").html());
+  var breakMinutes = parseInt($("#breakLength").html());
+  var sessionRunning = false;
+  var breakRunning = false;
+  var sessionSeconds = 0;
+  var breakSeconds = 0;
 
-function increaseSession() {
-  if (!activeSession) {
-    var value = parseInt(document.getElementById('sessionLength').innerHTML);
-    value++;
-    sessionMinutes = value;
-    document.getElementById('sessionLength').innerHTML = value;
-  }
-}
-
-function decreaseSession() {
-  if (!activeSession) {
-    var value = parseInt(document.getElementById('sessionLength').innerHTML);
-    if (value !== 0) {
-      value--;
-      sessionMinutes = value;
-      document.getElementById('sessionLength').innerHTML = value;
+  $("#start").on("click", function() {
+    if(!sessionRunning) {
+      sessionRunning = true;
+      $(this).val("Pause");
+    } else {
+      sessionRunning = false;
+      $(this).val("Resume");
     }
-  }
-}
+  });
 
-function increaseBreak() {
-  if (!activeBreak) {
-    var value = parseInt(document.getElementById('breakLength').innerHTML);
-    value++;
-    breakMinutes = value;
-    document.getElementById('breakLength').innerHTML = value;
-  }
-}
-
-function decreaseBreak() {
-  if (!activeBreak) {
-    var value = parseInt(document.getElementById('breakLength').innerHTML);
-    if (value !== 0) {
-      value--;
-      breakMinutes = value;
-      document.getElementById('breakLength').innerHTML = value;
+  $("#reset").on("click", function() {
+    if (!sessionRunning) {
+      sessionMinutes = $("#sessionLength").html();
+      sessionSeconds = 0;
+      $("#clock").html(sessionMinutes + ":" + "0" + sessionSeconds);
+      $("#start").val("Start");
     }
-  }
-}
+  });
 
-function startToPause() {
-  if (!activeSession) {
-    activeSession = true;
-    clockResetToggle();
-    document.getElementById('startToPause').value = "Pause";
-    runClock();
-  }
-  else {
-    activeSession = false;
-    clockResetToggle();
-    document.getElementById('startToPause').value = "Resume";
-  }
-}
+  $("#increaseSession").on("click", function() {
 
-function clockResetToggle() {
-  if (activeSession) {
-    document.getElementById('resetClock').classList.add('disabled');
-  } else {
-    document.getElementById('resetClock').classList.remove('disabled');
-  }
-}
+  });
 
-function resetClock() {
-  if (!activeSession) {
-    sessionMinutes = parseInt(document.getElementById('sessionLength').innerHTML);
-    sessionSeconds = 0;
-    document.getElementById('clock').innerHTML = sessionMinutes + ":" + "0" + sessionSeconds;
-    document.getElementById('startToPause').value = "Start";
-  }
-}
+  $("#decreaseSession").on("click", function () {
 
-function runClock() {
-  if (activeSession) {
-    setTimeout(countdown, 1000);
-  }
-}
+  });
 
-function countdown() {
-  if (sessionSeconds === 0) {
-    if (sessionMinutes === 0) {
-      activeSession = false;
-      activeBreak = true;
-      runBreak();
-      return;
-    }
-    breakMinutes = parseInt(document.getElementById('breakLength').innerHTML);
-    sessionMinutes--;
-    sessionSeconds = 59;
-  } else {
-    sessionSeconds--;
-  }
-  if (sessionSeconds < 10) {
-    document.getElementById('clock').innerHTML = sessionMinutes + ":" + "0" + sessionSeconds;
-  } else {
-    document.getElementById('clock').innerHTML = sessionMinutes + ":" + sessionSeconds;
-  }
-runClock();
-}
+  $("#increaseBreak").on("click", function () {
+
+  });
+
+  $("#decreaseBreak").on("click", function () {
+
+  });
+
+});
