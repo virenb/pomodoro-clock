b68898f14d14dd9c952b05f52bc72343c5e1ce21
Viren Bhagat <virenb@gmail.com>
2017-10-24 23:46:46 -0400

    Need to fix logic for break session pausing and resuming

--- app.js
+++ app.js
@@ -8,38 +8,134 @@ $(document).ready(function() {
   var breakSeconds = 0;
 
   $("#start").on("click", function() {
-    if(!sessionRunning) {
+    if (!sessionRunning) {
       sessionRunning = true;
+      resetToggle();
+      countdown();
       $(this).val("Pause");
     } else {
       sessionRunning = false;
+      resetToggle();
+      $(this).val("Resume");
+    }
+  });
+
+  $("#startBreak").on("click", function () {
+    if (!breakRunning) {
+      breakRunning = true;
+      runBreak();
+      $(this).val("Pause");
+    } else {
+      breakRunning = false;
       $(this).val("Resume");
     }
   });
 
   $("#reset").on("click", function() {
     if (!sessionRunning) {
-      sessionMinutes = $("#sessionLength").html();
+      sessionMinutes = parseInt($("#sessionLength").html());
       sessionSeconds = 0;
       $("#clock").html(sessionMinutes + ":" + "0" + sessionSeconds);
       $("#start").val("Start");
     }
   });
 
-  $("#increaseSession").on("click", function() {
+  function resetToggle() {
+    if (sessionRunning) {
+      $("#reset").addClass('.disabled');
+    } else {
+      $("#reset").removeClass('.disabled');
+    }
+  }
 
+  function countdown() {
+    if (sessionRunning) {
+      setTimeout(function () {
+        if (sessionSeconds === 0) {
+          if (sessionMinutes === 0) {
+            sessionRunning = false;
+            breakRunning = true;
+            runBreak();
+            return;
+          }
+          breakMinutes = parseInt($("#breakLength").html());
+          sessionMinutes--;
+          sessionSeconds = 59;
+        } else {
+          sessionSeconds--;
+        }
+
+        if (sessionSeconds < 10) {
+          $("#clock").html(sessionMinutes + ":" + "0" + sessionSeconds);
+        } else {
+          $("#clock").html(sessionMinutes + ":" + sessionSeconds);
+        }
+        countdown();
+      }, 1000);
+    }
+  }
+
+
+  $("#increaseSession").on("click", function() {
+    if (!sessionRunning) {
+      var value = parseInt($("#sessionLength").html());
+      value++;
+      sessionMinutes = value;
+      $("#sessionLength").html(value);
+    }
   });
 
   $("#decreaseSession").on("click", function () {
-
+    if (!sessionRunning) {
+      var value = parseInt($("#sessionLength").html());
+      if (value !== 0) {
+        value--;
+        sessionMinutes = value;
+        $("#sessionLength").html(value);
+      }
+    }
   });
 
-  $("#increaseBreak").on("click", function () {
+  function runBreak() {
+      setTimeout(function () {
+        if (breakSeconds === 0) {
+          if (breakMinutes === 0) {
+            breakRunning = false;
+            resetToggle();
+            return;
+          }
+          breakMinutes--;
+          breakSeconds = 59;
+        } else {
+          breakSeconds--;
+        }
+        if (breakSeconds < 10) {
+          $("#clock").html(breakMinutes + ":" + "0" + breakSeconds);
+        } else {
+          $("#clock").html(breakMinutes + ":" + breakSeconds);
+        }
+        runBreak();
+      }, 1000);
+  }
 
+  $("#increaseBreak").on("click", function () {
+    if (!breakRunning) {
+      var value = parseInt($("#breakLength").html());
+      value++;
+      breakMinutes = value;
+      $("#breakLength").html(value);
+    }
   });
 
   $("#decreaseBreak").on("click", function () {
-
+    if (!breakRunning) {
+      var value = parseInt($("#breakLength").html());
+      if (value !== 0) {
+        value--;
+        breakMinutes = value;
+        $("#breakLength").html(value);
+      }
+    }
   });
 
 });
